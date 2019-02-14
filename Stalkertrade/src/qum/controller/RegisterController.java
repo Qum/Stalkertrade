38b49064f3e3dd983927c8a903fcb475387af9ed
Dima <enigma12330@gmail.com>
2019-02-15 00:04:36 +0200

    init commit

--- Stalkertrade/src/qum/controller/RegisterController.java
+++ Stalkertrade/src/qum/controller/RegisterController.java
@@ -19,7 +19,7 @@ public class RegisterController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     public RegisterController() {
-	super(); 
+	super();  
     } 
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
